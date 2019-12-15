f18116b1128a929a02f84c7d3958db94143452bf
Henrique Fernandes <henrique.fquick@gmail.com>
2019-12-14 23:37:25 -0300

    Adicionando funções ao código e correções

--- calculadora.sh
+++ calculadora.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 
 #Programa simples para calculos de operacoes basicas para conhecer um pouco mais do shell
+function testarOperacoes {
+	if [[ ${RESP} != "" ]]
+	then
+		fluxoSemArgumentos
+	fi	
+	case ${OPERACAO} in
+		"0" | "+")
+			RESULT=${VALOR1}" + "${VALOR2}" = "$((VALOR1+VALOR2))
+			echo "O resultado da adicao de ${VALOR1} com ${VALOR2} eh: $((VALOR1+VALOR2))"
+			;;
+		"1" | "-")
+			RESULT=${VALOR1}" - "${VALOR2}" = "$((VALOR1-VALOR2))
+			echo "O valor da subtracao de ${VALOR1} com ${VALOR2} eh: $((VALOR1-VALOR2))"
+			;;
+		"2" | "*")
+			RESULT=${VALOR1}" * "${VALOR2}" = "$((VALOR1*VALOR2))
+			echo "O valor da multiplicacao de ${VALOR1} com ${VALOR2} eh: $((VALOR1*VALOR2))"
+			;;
+		"3" | "/")
+			RESULT=${VALOR1}" / "${VALOR2}" = "$((VALOR1/VALOR2))
+			echo "O valor da divisao de ${VALOR1} com ${VALOR2} eh: $((VALOR1/VALOR2))"
+			;;
+		*)
+			echo "erro"
+			;;
+	esac
+}
+
+function fluxoSemArgumentos {
+	read -p "Qual o primeiro valor? " VALOR1
+	read -p "Qual o segundo valor? " VALOR2
+
+	read -p "Qual a operacao?
+	0 - Adicao
+	1 - Subtracao
+	2 - Multiplicacao
+	3 - Divisao 
+	OPCAO :    " OPERACAO
+}
+
 VALOR1=$1
 OPERACAO=$2
 VALOR2=$3
@@ -15,49 +55,32 @@ do
 		ERRO=false
 		if [[ $# -eq 0 ]]
 		then
-			echo "Qual o primeiro valor?"
-			read VALOR1
-			echo "Qual o segundo valor?"
-			read VALOR2
-		
-			echo "Qual a operacao?"
-			echo -e "0 - Adicao\n1 - Subtracao\n2 - Multiplicacao\n3 - Divisao"
-		    read OPERACAO	
-	    	fi
-		case ${OPERACAO} in
-			"0" | "+")
-				RESULT=${VALOR1}" + "${VALOR2}" = "$((VALOR1+VALOR2))
-				echo "O resultado da adicao de ${VALOR1} com ${VALOR2} eh: $((VALOR1+VALOR2))"
-				;;
-			"1" | "-")
-				RESULT=${VALOR1}" - "${VALOR2}" = "$((VALOR1-VALOR2))
-				echo "O valor da subtracao de ${VALOR1} com ${VALOR2} eh: $((VALOR1-VALOR2))"
-				;;
-			"2" | "*")
-				RESULT=${VALOR1}" * "${VALOR2}" = "$((VALOR1*VALOR2))
-				echo "O valor da multiplicacao de ${VALOR1} com ${VALOR2} eh: $((VALOR1*VALOR2))"
-				;;
-			"3" | "/")
-				RESULT=${VALOR1}" / "${VALOR2}" = "$((VALOR1/VALOR2))
-				echo "O valor da divisao de ${VALOR1} com ${VALOR2} eh: $((VALOR1/VALOR2))"
-				;;
-			*)
-				echo "Codigo de operacao invalida."
-				ERRO=true
-		esac
-		if [[ ! -d "resultados" ]]
+			RESP=""
+			fluxoSemArgumentos
+			RESP=`testarOperacoes`
+		else
+			RESP=`testarOperacoes`
+		fi
+		if [[ ${RESP} != "erro" ]]
+		then
+			echo ${RESP}
+		else
+			ERRO=true
+			echo "Operacao Invalida"
+		fi	
+		if [[ ! -d "resultados" ]] && [[ ${ERRO} = false ]]
 		then
 			mkdir resultados
 		fi
 		cd resultados
-		echo ${RESULT} > resultados.txt
-	echo ${ERRO}
+		echo -e "${RESP}\n" >> resultados.txt
 	done	
-	echo -e  "Deseja realizar outra operacao?\nDigite (s) para Sim e (n) para Nao"
+	echo -e "Deseja realizar outra operacao?\nDigite (s) para Sim e (n) para Nao"
 	read RES
 	if [[ ${RES} = "n" ]]
 	then
 		AGAIN=false
 	fi
+	cd ..
 done
 echo "Obrigado por usar nosso programa :)"
